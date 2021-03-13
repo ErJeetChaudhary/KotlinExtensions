c3f81d743051b0e1d9d2f9e6aa4aee861bd825c9
Jitendra Singh <jeetsiem@gmail.com>
2021-03-13 11:21:40 +0530

    Update CommanExtensions.kt

--- CommanExtensions.kt
+++ CommanExtensions.kt
@@ -78,3 +78,20 @@ internal fun Spinner.setEntries(@ArrayRes arrayId: Int) {
         setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
     }
 }
+
+internal fun TextView.makeLinks(vararg links: Pair<String, View.OnClickListener>) {
+    val spannableString = SpannableString(this.text)
+    for (link in links) {
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(view: View) {
+                Selection.setSelection((view as TextView).text as Spannable, 0)
+                view.invalidate()
+                link.second.onClick(view)
+            }
+        }
+        val startIndexOfLink = this.text.toString().indexOf(link.first)
+        spannableString.setSpan(clickableSpan, startIndexOfLink, startIndexOfLink + link.first.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+    }
+    this.movementMethod = LinkMovementMethod.getInstance() // without LinkMovementMethod, link can not click
+    this.setText(spannableString, TextView.BufferType.SPANNABLE)
+}
