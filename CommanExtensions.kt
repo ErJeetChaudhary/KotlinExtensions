671b5e9de5d0f536e7bfa63695a5f341416dc334
Jitendra Singh <jeetsiem@gmail.com>
2021-03-13 11:23:18 +0530

    Update CommanExtensions.kt

--- CommanExtensions.kt
+++ CommanExtensions.kt
@@ -95,3 +95,131 @@ internal fun TextView.makeLinks(vararg links: Pair<String, View.OnClickListener>
     this.movementMethod = LinkMovementMethod.getInstance() // without LinkMovementMethod, link can not click
     this.setText(spannableString, TextView.BufferType.SPANNABLE)
 }
+
+inline fun CollapsingToolbarLayout.hideTitleOnExpand(appBarLayout: AppBarLayout, title: String, crossinline action: (offset: Int) -> Unit = { _ -> }) {
+    appBarLayout.addOnOffsetChangedListener(object : AppBarLayout.OnOffsetChangedListener {
+
+        var isShow = true
+
+        var scrollRange = -1
+
+        override fun onOffsetChanged(appBarLayout: AppBarLayout?, verticalOffset: Int) {
+            action(verticalOffset)
+            if (scrollRange == -1) {
+                scrollRange = appBarLayout?.totalScrollRange ?: 0
+            }
+
+            if (scrollRange + verticalOffset == 0) {
+                this@hideTitleOnExpand.title = title
+                isShow = true
+            } else if (isShow) {
+                this@hideTitleOnExpand.title = " " // careful there should a space between double quote otherwise it wont work
+                isShow = false
+            }
+        }
+    })
+}
+
+internal fun EditText.textWatcher(action: (s: String?) -> Unit) {
+    this.addTextChangedListener(object : TextWatcher {
+
+        override fun afterTextChanged(s: Editable?) {}
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            action(s?.toString())
+        }
+    })
+}
+
+internal fun ViewPager.onPageChanged(action: (position: Int) -> Unit) {
+    this.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+
+        override fun onPageScrollStateChanged(state: Int) {}
+
+        override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+
+        override fun onPageSelected(position: Int) {
+            action(position)
+        }
+    })
+}
+
+internal fun ViewPager2.onPageChanged(action: (position: Int) -> Unit) {
+    this.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+
+        override fun onPageScrollStateChanged(state: Int) {}
+
+        override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+
+        override fun onPageSelected(position: Int) {
+            action(position)
+        }
+
+    })
+}
+
+internal fun RadioGroup.onCheckChanged(action: (group: RadioGroup?, checkedId: Int) -> Unit) {
+    this.setOnCheckedChangeListener { group, checkedId -> action(group, checkedId) }
+}
+
+internal fun NestedLinearRadioGroup.onCheckChanged(action: (group: NestedRadioGroupManager?, checkedId: Int) -> Unit) {
+    this.setOnCheckedChangeListener { group, checkedId -> action(group, checkedId) }
+}
+
+internal fun ScrollView.isVisible(view: View): Boolean {
+    val scrollBounds = Rect()
+    this.getDrawingRect(scrollBounds)
+    val top = view.y
+    val bottom = top + view.height
+    return scrollBounds.top < top && scrollBounds.bottom > bottom // fully visible
+}
+
+inline fun View.doOnTouch(crossinline action: (view: View?, event: MotionEvent?) -> Boolean) {
+    this.setOnTouchListener { v, event -> action(v, event) }
+}
+
+inline fun View.startAnimation(
+    @AnimRes anim: Int,
+    crossinline onAnimationStart: (animation: Animation?) -> Unit = { },
+    crossinline onAnimationRepeat: (animation: Animation?) -> Unit = { },
+    crossinline onAnimationEnd: (animation: Animation?) -> Unit = { }
+) {
+    val animation = AnimationUtils.loadAnimation(this.context, anim)
+    animation.onAnimationStart(onAnimationStart)
+    animation.onAnimationEnd(onAnimationEnd)
+    animation.onAnimationRepeat(onAnimationRepeat)
+    this.startAnimation(animation)
+}
+
+inline fun Animation.onAnimationRepeat(crossinline action: (animation: Animation?) -> Unit) = addOnAnimationListener(onAnimationRepeat = action)
+
+inline fun Animation.onAnimationEnd(crossinline action: (animation: Animation?) -> Unit) = addOnAnimationListener(onAnimationEnd = action)
+
+inline fun Animation.onAnimationStart(crossinline action: (animation: Animation?) -> Unit) = addOnAnimationListener(onAnimationStart = action)
+
+inline fun Animation.addOnAnimationListener(
+    crossinline onAnimationRepeat: (animation: Animation?) -> Unit = { },
+    crossinline onAnimationEnd: (animation: Animation?) -> Unit = { },
+    crossinline onAnimationStart: (animation: Animation?) -> Unit = { }
+): Animation.AnimationListener {
+
+    val listener = object : Animation.AnimationListener {
+
+        override fun onAnimationRepeat(animation: Animation?) {
+            onAnimationRepeat(animation)
+        }
+
+        override fun onAnimationEnd(animation: Animation?) {
+            onAnimationEnd(animation)
+        }
+
+        override fun onAnimationStart(animation: Animation?) {
+            onAnimationStart(animation)
+        }
+    }
+
+    setAnimationListener(listener)
+    return listener
+}
